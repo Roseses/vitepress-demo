46f9fd4427243c2e364578b0e5decb6d73f3e43b
YeZhengXiong <382611619@qq.com>
2023-08-30 23:52:52 +0800

    feat: 优化导航栏与侧边栏

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,6 +6,7 @@ module.exports = {
     lang: 'zh-CN',
     head: [['link', { rel: 'icon', type: 'image/svg+xml', href: 'icon.svg' }]],
     themeConfig: {
+        logo: '/icon.svg',
         socialLinks: [
           { icon: 'github', link: 'https://github.com/Roseses/vitepress-demo.git' }
         ],
@@ -13,22 +14,60 @@ module.exports = {
             message: '其实我也不知道写啥注脚',
             copyright: '那我就随便写个吧',
         },
-        sidebar: [
+        nav: [
+          { text: 'Guide', link: '/guide/' },
+          {
+            text: 'components',
+            items: [
+              { text: 'com A', link: '/components/use' }
+            ]
+          }
+        ],
+        sidebar: {
+          '/guide/': [
             {
               text: "基础",
+              collapsed: true,
               items: [
                 { text: "快速开始", link: "/guide/" },
                 { text: "使用方式", link: "/guide/use" }
-                ]
+              ]
             },
             {
+              text: "高级",
+              collapsed: true,
+              items: [
+                { text: "快速开始", link: "/guide/" },
+                { text: "使用方式", link: "/guide/use" }
+              ]
+            }
+          ],
+          '/components/': [
+                        {
                 text: "组件",
                 items: [
                   { text: "快速开始", link: "/components/" },
                   { text: "使用方式", link: "/components/use" }
                   ]
               },
-        ],
+          ]
+        }
+        // sidebar: [
+        //     {
+        //       text: "基础",
+        //       items: [
+        //         { text: "快速开始", link: "/guide/" },
+        //         { text: "使用方式", link: "/guide/use" }
+        //         ]
+        //     },
+        //     {
+        //         text: "组件",
+        //         items: [
+        //           { text: "快速开始", link: "/components/" },
+        //           { text: "使用方式", link: "/components/use" }
+        //           ]
+        //       },
+        // ],
     },
     // markdown: {
     //   theme: {
