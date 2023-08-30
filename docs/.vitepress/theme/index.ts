a310547dae7a844ac5f3e86d655f69a01552fa1d
YeZhengXiong <382611619@qq.com>
2023-08-30 17:30:36 +0800

    feat: 首页标题名优化

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,13 @@
 import Theme from 'vitepress/theme'
-import './style/var.css'
-;
+import './style/var.css';
+import { h } from "vue";
+import AnimateTitle from "./components/AnimateTitle.vue";
+
 export default {
-  ...Theme
+  ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+      "home-hero-info": () => h(AnimateTitle),
+    });
+  },
 }
